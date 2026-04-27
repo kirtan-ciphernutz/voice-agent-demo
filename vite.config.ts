da3f19b0f6a21d7e39784c7390ddbf2d1506002f
Kirtan <kirtan@ciphernutz.com>
2026-04-27 18:01:03 +0530

    Configure DNS to prioritize IPv4
    Set default DNS result order to IPv4 first.

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import dns from 'node:dns'
+
+dns.setDefaultResultOrder('ipv4first')
 
 // https://vite.dev/config/
 export default defineConfig({
